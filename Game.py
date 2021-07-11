6d7e4b6e3ab4b7f43d617d04c6ff29ab43e45c88
Lucas Gois <lucas.gois@linnks.com.br>
2021-07-11 14:53:07 -0300

    game

---
+++ Game.py
@@ -0,0 +1,69 @@
+import os
+
+
+def clear():
+    return os.system('cls' if os == 'nt' else 'clear')
+
+
+class Game:
+    new_letter: str = None
+    correct_letters = set()
+    incorrect_letters = set()
+    end: bool = False
+    win: bool = False
+
+    man = ['|      ',
+           '|      O \n',
+           '|      O \n|      | ',
+           '|      O \n|     /| ',
+           '|      O \n|     /|\ ',
+           '|      O \n|     /|\ \n|      /  ',
+           '|      O \n|     /|\ \n|      /\ ',
+           ]
+
+    def __init__(self, word):
+        self.word: str = word
+
+    def render(self):
+        clear()
+        print('********** Jogo da Forca ***********')
+        print('+------+')
+        print('|      |')
+        print(self.man[len(self.incorrect_letters)])
+        print('|       ')
+        print('=============\n\n')
+        state = ''
+        for letter in self.word:
+            if letter in self.correct_letters:
+                state += letter
+            else:
+                state += '_'
+        print(f'Palavra: {state}\n')
+        print(f"Letras erradas: {[x for x in self.incorrect_letters]}\n")
+        print(f"Letras corretas: {[x for x in self.correct_letters]}\n")
+
+    def set_letter(self, letter: str):
+        self.new_letter = letter
+        self.update_game()
+
+    def update_game(self):
+        if self.new_letter in self.word:
+            self.correct_letters.add(self.new_letter)
+        else:
+            self.incorrect_letters.add(self.new_letter)
+
+    def validate_game(self):
+        self.win = True
+        for letter in self.word:
+            if letter not in self.correct_letters:
+                self.win = False
+        if self.win:
+            self.end = True
+        if len(self.incorrect_letters) == len(self.man):
+            self.end = True
+
+    def result(self):
+        if self.win:
+            print(f'VOCÊ VENCEU !!!! A palavra é {self.word}')
+        else:
+            print(f'Que pena, foi enforcado!! :(  A palavra era {self.word}')
---
+++ jogo_da_forca.py
@@ -0,0 +1,19 @@
+import random
+from Game import Game
+
+if __name__ == '__main__':
+    with open('words.txt', 'r') as doc:
+        words = doc.read().split(',')
+
+    choose_word = random.choice(words)
+
+    new_game = Game(choose_word)
+    while not new_game.end:
+        new_game.render()
+        letter = input('Digite uma letra: ')
+        new_game.set_letter(letter)
+        new_game.validate_game()
+
+    new_game.result()
+
+
